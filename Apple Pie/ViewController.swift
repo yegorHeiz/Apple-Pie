6ba1378dff94f6ef4fc36bd1d9fff5d0f4e7567a
yegorheiz <yegorheiz@gmail.com>
2020-07-16 12:52:33 +0300

    Initial Commit

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Yegor Heiz on 16.07.2020.
+//  Copyright © 2020 Yegor Heiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
